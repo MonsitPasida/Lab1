9f6ce6ab9d97a1c54073f13345caf840a2bcf848
MonsitPasida <53083364+MonsitPasida@users.noreply.github.com>
2020-03-15 11:24:39 +0700

    first commit

---
+++ src/lab1/Main.java
@@ -0,0 +1,14 @@
+
+package lab1;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        System.out.println("MONSIT PASIDA");
+        System.out.println("นายมนต์สิทธิ์ ผาสีดา");
+        System.out.println("รหัส 0030050");
+    }
+    
+}
